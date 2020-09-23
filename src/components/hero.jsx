4df231c80ef6cba5f94c8fcd147953fbbe8e3a2b
Malakhov Dmitriy <dmitriy.malakhov@gorparkovki.ru>
2020-09-23 16:11:32 +0300

    gh-pages

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,8 @@ import "../styles/style.scss"
 const Hero = () => {
   return (
     <div className="app">
-      <div className="title">FFMEMES Bot</div>
+      <div className="title">FFmemes</div>
+      <div className="sub_title">infnite personalized meme feed</div>
       <div className="split-zone">
         <div className="left-side">
           <button>Open in Messenger</button>
