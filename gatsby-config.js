2dfb00a980978fbcd244967f47ab6f26deac90a4
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2020-09-25 14:35:24 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: "/ffmemes_v2",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `FFmemesbot`,
+    description: `Get personalized meme feed in Facebook Messenger and Telegram chatbots. FFmemes shows you the memes you like based on your and other users likes and dislikes. We scrape top memes from various sources and our moderators manually filter out low quality content.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
