24bed92e2a555f021d12585d70ceeb5fd4064f56
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2020-09-25 14:22:50 +0300

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist... the sadness. Maybe you tried to find our Telegram bot: https://t.me/ffmemesbot/</p>
   </>
 )
 
