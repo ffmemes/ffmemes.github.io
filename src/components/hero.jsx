16c53115be4d7cd4d4d13d7f7016185a393362c0
Daniil Okhlopkov <5613295+ohld@users.noreply.github.com>
2022-11-25 15:50:38 +0400

    remove facebook link

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,6 @@
 import React from "react"
 import "../styles/style.scss"
-// import { TgImage, MsgImage } from "./image"
 import tgLogo from "../images/tg.png"
-import msgLogo from "../images/messenger.png"
 
 const Hero = () => {
   return (
@@ -14,34 +12,18 @@ const Hero = () => {
         </div>
       </div>
       <div className="split-zone">
-        <div className="left-side">
-          <button>
-            <span>
-              <a
-                className="ref-link"
-                href="https://m.me/ffmemesbot"
-                target="blank"
-              >
-                Open in <b>Messenger</b>
-              </a>
-            </span>
-            <img className="app-logo" src={msgLogo} />
-          </button>
-        </div>
-        <div className="right-side">
-          <button>
-            <span>
-              <a
-                className="ref-link"
-                href="https://t.me/ffmemesbot?start=ffmemescom"
-                target="blank"
-              >
-                Open in <b>Telegram</b>
-              </a>
-            </span>
-            <img className="app-logo" src={tgLogo} />
-          </button>
-        </div>
+        <button>
+          <span>
+            <a
+              className="ref-link"
+              href="https://t.me/ffmemesbot?start=ffmemescom"
+              target="blank"
+            >
+              Open in <b>Telegram</b>
+            </a>
+          </span>
+          <img className="app-logo" src={tgLogo} />
+        </button>
       </div>
     </div>
   )
